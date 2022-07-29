a03b20f9d87437df5dee13168977da3233db760f
Pierre Smit <pierre.smit@outlook.com>
2022-07-29 14:25:09 +0200

    To Version: 0.1.17

--- PSSysTray/Public/csvtojson.ps1
+++
@@ -1,2 +0,0 @@
-﻿$config = Get-Content $PSSysTrayConfigFile | Where-Object {$_ -notlike '##*'} | ConvertFrom-Csv -Delimiter '~'
-$config | where {$_.mainmenu -like "ModuleBuild"} | select Name,Command,Arguments,Mode,Window,RunAsAdmin | ConvertTo-Json | Out-Clipboard
